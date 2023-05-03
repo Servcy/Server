a0a4c4d7f4bbb6cd1894355e2773136c0ddcf440
Megham Garg <gargmegham3@gmail.com>
2023-05-03 20:06:42 +0530

    remove admin from urls

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -6,7 +5,6 @@ from .apis.auth import Authentication
 from .apis.logout import LogoutView
 
 urlpatterns = [
-    path("admin", admin.site.urls),
     path("authentication", Authentication.as_view(), name="AuAuthentication Manager"),
     path("refresh-token", TokenRefreshView.as_view(), name="Refresh Token"),
     path("logout", LogoutView.as_view(), name="Auth Logout"),
