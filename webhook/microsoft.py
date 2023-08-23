de2cc5dfbd6842859cae40edfcc8c8df0060e8d8
Megham Garg <gargmegham3@gmail.com>
2023-08-23 19:41:07 +0530

    json load and handle no integration

--- webhook/microsoft.py
+++ webhook/microsoft.py
@@ -6,7 +6,9 @@ from django.db import transaction
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from rest_framework import status
 
+from common.responses import error_response
 from inbox.repository import InboxRepository
 from inbox.repository.microsoft import OutlookMailRepository
 from integration.repository import IntegrationRepository
@@ -25,19 +27,26 @@ def microsoft(request):
                 content=validation_token, content_type="text/plain", status=200
             )
         notificaiton = json.loads(request.body.decode("utf-8"))
-        account_id = int(notificaiton["value"][0]["clientState"])
+        account_id = notificaiton["value"][0]["clientState"]
         message_id = notificaiton["value"][0]["resourceData"]["id"]
-        user_integration = IntegrationRepository.get_user_integrations(
-            {"integration__name": "Outlook", "account_id": account_id}
+        integration = IntegrationRepository.get_user_integrations(
+            {"integration__name": "Outlook", "account_id": account_id},
+            first=True,
         )
-        service = MicrosoftService(
-            refresh_token=user_integration["meta_data"]["token"]["refresh_token"]
-        )
+        if integration is None:
+            logger.error(f"No integration found for outlook: {account_id}")
+            return error_response(
+                logger=logger,
+                logger_message="No integration found for email.",
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        tokens = json.loads(integration["meta_data"]["token"].replace("'", '"'))
+        service = MicrosoftService(refresh_token=tokens["refresh_token"])
         mail = service.get_message(message_id)
         with transaction.atomic():
             inbox_items = OutlookMailRepository.create_mail(
                 mail=mail,
-                user_integration_id=user_integration["id"],
+                user_integration_id=integration["id"],
             )
             InboxRepository.add_items(inbox_items)
         return HttpResponse(status=200)
