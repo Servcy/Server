d4bd427389c6ef219fd8905ab0e6d8b1e0c6550f
Megham Garg <gargmegham3@gmail.com>
2023-08-24 08:34:55 +0530

    mail create bug fix

--- inbox/repository/google.py
+++ inbox/repository/google.py
@@ -1,10 +1,8 @@
 import datetime
 
-from django.db import IntegrityError
 from django.utils import timezone
 
 from inbox.models import GoogleMail
-from inbox.services.google import GoogleMailService
 
 
 class GoogleMailRepository:
@@ -14,37 +12,23 @@ class GoogleMailRepository:
 
     @staticmethod
     def create_mails(mails: list, user_integration_id: int) -> list[dict]:
-        inbox_items = []
+        mail_objects = []
         for mail in mails:
-            mail_object = GoogleMail(
-                thread_id=mail["threadId"],
-                history_id=mail["historyId"],
-                message_id=mail["id"],
-                snippet=mail["snippet"],
-                size_estimate=mail["sizeEstimate"],
-                label_ids=mail["labelIds"],
-                payload=mail["payload"],
-                internal_date=timezone.make_aware(
-                    datetime.datetime.fromtimestamp(int(mail["internalDate"]) / 1000)
-                ),
-                user_integration_id=user_integration_id,
-            )
-            try:
-                mail_object.save()
-            except IntegrityError:
-                continue
-            inbox_items.append(
-                {
-                    "title": GoogleMailService._get_mail_header(
-                        "Subject", mail["payload"]["headers"]
+            mail_objects.append(
+                GoogleMail(
+                    thread_id=mail["threadId"],
+                    history_id=mail["historyId"],
+                    message_id=mail["id"],
+                    snippet=mail["snippet"],
+                    size_estimate=mail["sizeEstimate"],
+                    label_ids=mail["labelIds"],
+                    payload=mail["payload"],
+                    internal_date=timezone.make_aware(
+                        datetime.datetime.fromtimestamp(
+                            int(mail["internalDate"]) / 1000
+                        )
                     ),
-                    "cause": GoogleMailService._get_mail_header(
-                        "From", mail["payload"]["headers"]
-                    ),
-                    "body": GoogleMailService._get_mail_body(mail["payload"]),
-                    "is_body_html": GoogleMailService._is_body_html(mail["payload"]),
-                    "user_integration_id": user_integration_id,
-                    "uid": f"{mail['id']}-{user_integration_id}",
-                }
+                    user_integration_id=user_integration_id,
+                )
             )
-        return inbox_items
+        return mail_objects
--- webhook/google.py
+++ webhook/google.py
@@ -2,7 +2,7 @@ import json
 import logging
 from base64 import decodebytes
 
-from django.db import transaction
+from django.db import IntegrityError, transaction
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from rest_framework import status
@@ -10,6 +10,7 @@ from rest_framework import status
 from common.responses import error_response, success_response
 from inbox.repository import InboxRepository
 from inbox.repository.google import GoogleMailRepository
+from inbox.services.google import GoogleMailService
 from integration.repository import IntegrationRepository
 from integration.services.google import GoogleService
 
@@ -24,14 +25,14 @@ def google(request):
         encoded_data = payload["message"]["data"]
         decoded_data = json.loads(decodebytes(encoded_data.encode()).decode())
         email = decoded_data["emailAddress"]
-        integrations = IntegrationRepository.get_user_integrations(
+        integration = IntegrationRepository.get_user_integrations(
             filters={
                 "account_id": email,
                 "integration__name": "Gmail",
             },
             first=True,
         )
-        if integrations is None:
+        if integration is None:
             logger.error(f"No integration found for email: {email}. Payload: {payload}")
             return error_response(
                 logger=logger,
@@ -39,26 +40,48 @@ def google(request):
                 status=status.HTTP_404_NOT_FOUND,
             )
         last_known_message = (
-            GoogleMailRepository.get_mails({"user_integration_id": integrations["id"]})
+            GoogleMailRepository.get_mails({"user_integration_id": integration["id"]})
             .order_by("-id")
             .first()
         )
         service = GoogleService(
-            token=integrations["meta_data"]["access_token"],
-            refresh_token=integrations["meta_data"]["refresh_token"],
+            token=integration["meta_data"]["access_token"],
+            refresh_token=integration["meta_data"]["refresh_token"],
         )
         new_messages = service.get_latest_unread_primary_inbox(
             last_known_message_id=last_known_message.message_id
             if last_known_message
             else None,
         )
+        inbox_items = []
+        mail_objects = GoogleMailRepository.create_mails(
+            mails=service.get_messages(
+                message_ids=[message["id"] for message in new_messages],
+            ),
+            user_integration_id=integration["id"],
+        )
         with transaction.atomic():
-            inbox_items = GoogleMailRepository.create_mails(
-                mails=service.get_messages(
-                    message_ids=[message["id"] for message in new_messages],
-                ),
-                user_integration_id=integrations["id"],
-            )
+            for mail in mail_objects:
+                try:
+                    mail.save()
+                    inbox_items.append(
+                        {
+                            "title": GoogleMailService._get_mail_header(
+                                "Subject", mail["payload"]["headers"]
+                            ),
+                            "cause": GoogleMailService._get_mail_header(
+                                "From", mail["payload"]["headers"]
+                            ),
+                            "body": GoogleMailService._get_mail_body(mail["payload"]),
+                            "is_body_html": GoogleMailService._is_body_html(
+                                mail["payload"]
+                            ),
+                            "user_integration_id": integration["id"],
+                            "uid": f"{mail['id']}-{integration['id']}",
+                        }
+                    )
+                except IntegrityError:
+                    pass
             InboxRepository.add_items(inbox_items)
         return success_response()
     except Exception:
