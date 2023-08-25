be6e895b9a6be343648d64d40d7ef3af6a84f6cc
Megham Garg <gargmegham3@gmail.com>
2023-08-25 16:14:39 +0530

    new relic wrapper

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,28 @@
 import os
+from configparser import RawConfigParser
+from pathlib import Path
 
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+CONFIG_FILE = BASE_DIR / "config/config.ini"
+
+NEW_RELIC_CONFIG_FILE = BASE_DIR / "config/new_relic.ini"
+
+config = RawConfigParser()
+
+config.read(CONFIG_FILE)
+
+HOST_TYPE = config.get("main", "host_type")
+
+DEBUG = HOST_TYPE == "development"
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
+
+if not DEBUG:
+    newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), "newrelic.ini"))
+    application = newrelic.agent.WSGIApplicationWrapper(application)
