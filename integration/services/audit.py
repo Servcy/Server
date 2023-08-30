d9bc9e7897ca5136f239c2b95ef1c7844cb3237e
Megham Garg <gargmegham3@gmail.com>
2023-08-30 17:31:53 +0530

    AuditAccess class

---
+++ integration/services/audit.py
@@ -0,0 +1,39 @@
+class AuditAccess:
+    """
+    In case an integration was revoked, we need to audit the access to the integration.
+    """
+
+    def google():
+        """
+        Audit the access to an integration.
+        """
+
+    def microsoft():
+        """
+        Audit the access to an integration.
+        """
+
+    def notion():
+        """
+        Audit the access to an integration.
+        """
+
+    def slack():
+        """
+        Audit the access to an integration.
+        """
+
+    def figma():
+        """
+        Audit the access to an integration.
+        """
+
+    def github():
+        """
+        Audit the access to an integration.
+        """
+
+    def main():
+        """
+        Audit the access to all integrations.
+        """
