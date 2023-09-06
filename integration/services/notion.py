7a7d8cf05139e72a340ff75866f12f5e91bd3859
Megham Garg <gargmegham3@gmail.com>
2023-09-06 17:03:29 +0530

    update notion integration

--- integration/services/notion.py
+++ integration/services/notion.py
@@ -14,6 +14,10 @@ NOTION_OAUTH_TOKEN_ENDPOINT = f"{NOTION_API_BASE_URL}/v1/oauth/token"
 class NotionService:
     """Service class for Notion integration."""
 
+    _NOTION_PAGE_SEARCH = "https://api.notion.com/v1/search"
+    _NOTION_BOT_USER = "https://api.notion.com/v1/users/me"
+    _NOTION_BLOCK_SEARCH = "https://api.notion.com/v1/blocks"
+
     def __init__(self, **kwargs) -> None:
         """Initialize the NotionService with an optional authorization code.
 
@@ -60,6 +64,7 @@ class NotionService:
 
         :param user_id: ID of the user for whom the integration is created
         """
+        pages = self.get_authorized_pages()
         return IntegrationRepository.create_user_integration(
             integration_id=IntegrationRepository.get_integration(
                 filters={"name": "Notion"}
@@ -68,6 +73,10 @@ class NotionService:
             account_id=self._token["workspace_id"],
             account_display_name=self._token["workspace_name"],
             meta_data={"token": self._token},
+            configuration={
+                "pages": pages,
+                "total": len(pages),
+            },
         )
 
     def is_active(self, meta_data, **kwargs):
@@ -89,3 +98,168 @@ class NotionService:
             },
         )
         return response.status_code == 200
+
+    def get_authorized_pages(self):
+        pages = []
+        page_results = self.page_search()
+        database_results = self.database_search()
+        # get page detail
+        for page_result in page_results:
+            page_id = page_result["id"]
+            if "Name" in page_result["properties"]:
+                if len(page_result["properties"]["Name"]["title"]) > 0:
+                    page_name = page_result["properties"]["Name"]["title"][0][
+                        "plain_text"
+                    ]
+                else:
+                    page_name = "Untitled"
+            elif "title" in page_result["properties"]:
+                if len(page_result["properties"]["title"]["title"]) > 0:
+                    page_name = page_result["properties"]["title"]["title"][0][
+                        "plain_text"
+                    ]
+                else:
+                    page_name = "Untitled"
+            elif "Title" in page_result["properties"]:
+                if len(page_result["properties"]["Title"]["title"]) > 0:
+                    page_name = page_result["properties"]["Title"]["title"][0][
+                        "plain_text"
+                    ]
+                else:
+                    page_name = "Untitled"
+            else:
+                page_name = "Untitled"
+            page_icon = page_result["icon"]
+            if page_icon:
+                icon_type = page_icon["type"]
+                if icon_type == "external" or icon_type == "file":
+                    url = page_icon[icon_type]["url"]
+                    icon = {
+                        "type": "url",
+                        "url": url
+                        if url.startswith("http")
+                        else f"https://www.notion.so{url}",
+                    }
+                else:
+                    icon = {"type": "emoji", "emoji": page_icon[icon_type]}
+            else:
+                icon = None
+            parent = page_result["parent"]
+            parent_type = parent["type"]
+            if parent_type == "block_id":
+                parent_id = self.block_parent_page_id(parent[parent_type])
+            elif parent_type == "workspace":
+                parent_id = "root"
+            else:
+                parent_id = parent[parent_type]
+            page = {
+                "page_id": page_id,
+                "page_name": page_name,
+                "page_icon": icon,
+                "parent_id": parent_id,
+                "type": "page",
+            }
+            pages.append(page)
+            # get database detail
+        for database_result in database_results:
+            page_id = database_result["id"]
+            if len(database_result["title"]) > 0:
+                page_name = database_result["title"][0]["plain_text"]
+            else:
+                page_name = "Untitled"
+            page_icon = database_result["icon"]
+            if page_icon:
+                icon_type = page_icon["type"]
+                if icon_type == "external" or icon_type == "file":
+                    url = page_icon[icon_type]["url"]
+                    icon = {
+                        "type": "url",
+                        "url": url
+                        if url.startswith("http")
+                        else f"https://www.notion.so{url}",
+                    }
+                else:
+                    icon = {"type": icon_type, icon_type: page_icon[icon_type]}
+            else:
+                icon = None
+            parent = database_result["parent"]
+            parent_type = parent["type"]
+            if parent_type == "block_id":
+                parent_id = self.block_parent_page_id(parent[parent_type])
+            elif parent_type == "workspace":
+                parent_id = "root"
+            else:
+                parent_id = parent[parent_type]
+            page = {
+                "page_id": page_id,
+                "page_name": page_name,
+                "page_icon": icon,
+                "parent_id": parent_id,
+                "type": "database",
+                "last_unresolved_comment": None,
+            }
+            pages.append(page)
+        return pages
+
+    def page_search(self):
+        data = {"filter": {"value": "page", "property": "object"}}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self._token['access_token']}",
+            "Notion-Version": "2022-06-28",
+        }
+        response = requests.post(
+            url=self._NOTION_PAGE_SEARCH, json=data, headers=headers
+        )
+        response_json = response.json()
+        if "results" in response_json:
+            results = response_json["results"]
+        else:
+            results = []
+        return results
+
+    def block_parent_page_id(self, block_id: str):
+        headers = {
+            "Authorization": f"Bearer {self._token['access_token']}",
+            "Notion-Version": "2022-06-28",
+        }
+        response = requests.get(
+            url=f"{self._NOTION_BLOCK_SEARCH}/{block_id}", headers=headers
+        )
+        response_json = response.json()
+        parent = response_json["parent"]
+        parent_type = parent["type"]
+        if parent_type == "block_id":
+            return self.block_parent_page_id(parent[parent_type])
+        return parent[parent_type]
+
+    def workspace_name(self):
+        headers = {
+            "Authorization": f"Bearer {self._token['access_token']}",
+            "Notion-Version": "2022-06-28",
+        }
+        response = requests.get(url=self._NOTION_BOT_USER, headers=headers)
+        response_json = response.json()
+        if "object" in response_json and response_json["object"] == "user":
+            user_type = response_json["type"]
+            user_info = response_json[user_type]
+            if "workspace_name" in user_info:
+                return user_info["workspace_name"]
+        return "workspace"
+
+    def database_search(self):
+        data = {"filter": {"value": "database", "property": "object"}}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self._token['access_token']}",
+            "Notion-Version": "2022-06-28",
+        }
+        response = requests.post(
+            url=self._NOTION_PAGE_SEARCH, json=data, headers=headers
+        )
+        response_json = response.json()
+        if "results" in response_json:
+            results = response_json["results"]
+        else:
+            results = []
+        return results
