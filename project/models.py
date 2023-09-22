ce9c31a8b80388922c81c05534c702d1c21ddb4a
Megham Garg <gargmegham3@gmail.com>
2023-09-22 16:09:01 +0530

    file_ids in projects

--- project/models.py
+++ project/models.py
@@ -8,6 +8,7 @@ class Project(TimeStampedModel):
     name = models.CharField(max_length=100)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    file_ids = models.JSONField(default=list)
 
     class Meta:
         db_table = "project"
--- project/serializers.py
+++ project/serializers.py
@@ -6,5 +6,5 @@ from project.models import Project
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = ["id", "name", "description", "created_at", "updated_at"]
+        fields = ["id", "name", "description", "file_ids", "created_at", "updated_at"]
         read_only_fields = ["id", "created_at", "updated_at"]
