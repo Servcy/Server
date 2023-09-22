b8606334e4ff7e5860faf73891c49e7de279adc7
Megham Garg <gargmegham3@gmail.com>
2023-09-22 15:39:20 +0530

    project files

--- project/models.py
+++ project/models.py
@@ -13,3 +13,14 @@ class Project(TimeStampedModel):
         db_table = "project"
         verbose_name = "Project"
         verbose_name_plural = "Projects"
+
+
+class ProjectFile(TimeStampedModel):
+    file = models.FileField(upload_to="ProjectFiles", null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
+    class Meta:
+        db_table = "project_file"
+        verbose_name = "Project File"
+        verbose_name_plural = "Project Files"
--- project/views/__init__.py
+++ project/views/__init__.py
@@ -3,6 +3,10 @@ import logging
 from rest_framework.viewsets import ModelViewSet
 
 from project.serializers import ProjectSerializer
+from project.models import ProjectFile
+from rest_framework.decorators import action
+from common.responses import error_response, success_response
+from rest_framework import status
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +18,30 @@ class ProjectViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=["post"], url_path="upload")
+    def upload(self, request):
+        """Upload a file to the project"""
+        try:
+            files = request.FILES.getlist("file")
+            user_id = request.user.id
+            file_ids = []
+            for file in files:
+                project_file = ProjectFile.objects.create(
+                    file=file,
+                    user_id=user_id,
+                    name=file.name,
+                )
+                file_ids.append(project_file.id)
+            return success_response(
+                results={"file_ids": file_ids},
+                status=status.HTTP_201_CREATED,
+            )
+        except Exception as ex:
+            logger.exception(
+                f"An unexpected error occurred processing project file upload request: {str(ex)}"
+            )
+            return error_response(
+                logger=logger,
+                logger_message="An unexpected error occurred processing project file upload request.",
+            )
