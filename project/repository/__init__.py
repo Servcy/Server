cca074dcef84f266fc0cad837ff41344fef48932
Megham Garg <gargmegham3@gmail.com>
2023-10-17 17:32:52 +0530

    ProjectRepository updated

--- project/repository/__init__.py
+++ project/repository/__init__.py
@@ -7,7 +7,13 @@ class ProjectRepository:
 
     def create(self, **kwargs) -> Project:
         """Creates a new project."""
-        return Project.objects.create(**kwargs)
+        return Project.objects.create(
+            name=kwargs["name"],
+            description=kwargs["description"],
+            uid=kwargs["uid"],
+            user_id=kwargs["user_id"],
+            user_integration_id=kwargs["user_integration_id"],
+        )
 
     def create_bulk(self, projects: list) -> list:
         """Creates bulk projects."""
