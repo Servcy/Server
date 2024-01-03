845b53cfc1a8ed78dc9bd0de92069d8939c38f76
Megham Garg <gargmegham3@gmail.com>
2024-01-03 22:27:30 +0530

    Add IntegrationEvent and DisabledUserIntegrationEvent models

--- integration/models.py
+++ integration/models.py
@@ -45,3 +45,51 @@ class UserIntegration(TimeStampedModel):
         verbose_name = "User Integration"
         verbose_name_plural = "User Integration"
         unique_together = ("user", "integration", "account_id")
+
+
+class IntegrationEvent(TimeStampedModel):
+    """
+    This model is used to keep track of unique events for an integration.
+    """
+
+    integration = models.ForeignKey(
+        Integration,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="integration_events",
+    )
+    event_type = models.CharField(max_length=250, null=False, blank=False)
+
+    class Meta:
+        db_table = "integration_event"
+        verbose_name = "Integration Event"
+        verbose_name_plural = "Integration Event"
+        unique_together = ("integration", "event_type")
+
+
+class DisabledUserIntegrationEvent(TimeStampedModel):
+    """
+    This model is used to keep track of disabled events for a user integration.
+    """
+
+    user_integration = models.ForeignKey(
+        UserIntegration,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="disabled_user_integration_events",
+    )
+    integration_event = models.ForeignKey(
+        IntegrationEvent,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="disabled_user_integration_events",
+    )
+
+    class Meta:
+        db_table = "disabled_user_integration_event"
+        verbose_name = "Disabled User Integration Event"
+        verbose_name_plural = "Disabled User Integration Event"
+        unique_together = ("user_integration", "integration_event")
