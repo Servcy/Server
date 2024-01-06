1a6c8df421d828c5218cfc4d639083a0f1ccb9df
Megham Garg <gargmegham3@gmail.com>
2024-01-06 10:43:39 +0530

    Add IntegrationEventViewSet to integration/urls.py

--- integration/urls.py
+++ integration/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from integration.views import IntegrationViewSet, UserIntegrationViewSet
+from integration.views import (
+    IntegrationViewSet,
+    UserIntegrationViewSet,
+    IntegrationEventViewSet,
+)
 from integration.views.oauth import OauthViewset
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register("oauth", OauthViewset, basename="oauth")
 router.register("user_integration", UserIntegrationViewSet, basename="user_integration")
+router.register(
+    "integration_event", IntegrationEventViewSet, basename="integration_event"
+)
 router.register("", IntegrationViewSet, basename="integration")
 
 urlpatterns = [
