aca00a4bbd28d5d48d1ef58caf936dafcdbea4ff
Megham Garg <gargmegham3@gmail.com>
2024-02-13 09:31:00 +0530

    Add method to bulk create documents

--- document/repository/document.py
+++ document/repository/document.py
@@ -5,3 +5,9 @@ class DocumentRepository:
     @staticmethod
     def create(**kwargs):
         return Document.objects.create(**kwargs)
+
+    @staticmethod
+    def add_documents(documents: list[dict]):
+        return Document.objects.bulk_create(
+            [Document(**document) for document in documents]
+        )
