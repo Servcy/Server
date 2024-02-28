8bea7efa545fe013971672634f4ebe4244e606d0
Megham Garg <gargmegham3@gmail.com>
2024-02-28 09:25:58 +0530

    Delete AssisstantViewSet and related code

--- inbox/urls.py
+++ inbox/urls.py
@@ -2,11 +2,9 @@ from django.urls import include, path
 from rest_framework import routers
 
 from inbox.views import InboxViewSet
-from inbox.views.assisstant import AssisstantViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register("", InboxViewSet, basename="inbox")
-router.register("assisstant", AssisstantViewSet, basename="assisstant")
 
 urlpatterns = [
     path("", include(router.urls)),
