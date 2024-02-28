566a70c2f409f6bc65b4d11f6347ad5623f4c6a0
Megham Garg <gargmegham3@gmail.com>
2024-02-28 13:51:46 +0530

    Remove unused code and methods in InboxViewSet

--- inbox/views/__init__.py
+++ inbox/views/__init__.py
@@ -5,11 +5,8 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 
 from common.responses import error_response, success_response
-from document.repository import DocumentRepository
 from inbox.repository import InboxRepository
 from inbox.services import InboxService
-from integration.repository import IntegrationRepository
-from integration.utils.maps import integration_service_map
 
 logger = logging.getLogger(__name__)
 
@@ -132,55 +129,4 @@ class InboxViewSet(ViewSet):
             return error_response(
                 logger=logger,
                 logger_message="Error while archiving inbox items",
-            )
-
-    @action(methods=["post"], detail=False, url_path="send-reply")
-    def send_reply(self, request):
-        try:
-            requesting_user = request.user
-            body = request.data.get("body", "")
-            reply = request.data.get("reply", "")
-            file_ids = request.data.get("file_ids", [])
-            removed_file_ids = request.data.get("removed_file_ids", [])
-            DocumentRepository.remove_documents(
-                [int(file_id) for file_id in removed_file_ids]
-            )
-            user_integration_id = request.data.get("user_integration_id", None)
-            is_body_html = request.data.get("is_body_html", False)
-            if not body or not reply or not user_integration_id:
-                return error_response(
-                    error_message="body, reply and user_integration_id are required to send reply",
-                    status=400,
-                )
-            user_integration = IntegrationRepository.get_user_integrations(
-                filters={"id": user_integration_id, "user_id": requesting_user.id},
-                first=True,
-                decrypt_meta_data=True,
-            )
-            if not user_integration:
-                return error_response(
-                    error_message="No user integration found, please make sure that integration hasn't been revoked.",
-                    status=400,
-                    logger=logger,
-                    logger_message="No user integration found for given id",
-                )
-            service_class = integration_service_map.get(
-                user_integration["integration__name"]
-            )
-            if service_class is None:
-                raise ValueError(
-                    f"Integration '{user_integration['integration__name']}' is not supported."
-                )
-            service_class.send_reply(
-                meta_data=user_integration["meta_data"],
-                user_integration=user_integration,
-                body=body,
-                reply=reply,
-                is_body_html=is_body_html,
-                file_ids=[int(file_id) for file_id in file_ids],
-            )
-            return success_response()
-        except Exception as e:
-            return error_response(
-                "Error while sending reply", logger=logger, logger_message=str(e)
             )
