a56d5a5ed83c141616f10eab257e5de65fe5be8f
Megham Garg <gargmegham3@gmail.com>
2024-02-28 10:06:06 +0530

    Remove unused import in client serializers

--- client/serializers.py
+++ client/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer
 
-from client.models import Avatar, Client
+from client.models import Client
 
 
 class ClientSerializer(ModelSerializer):
