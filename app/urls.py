12bb76083ccbb7c88e83dc2f6dce3332855234e7
Megham Garg <gargmegham3@gmail.com>
2024-03-06 12:58:01 +0530

    Update logout URL to use "sign-out" instead of "logout"

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from app.views.logout import LogoutView
 urlpatterns = [
     path("authentication", AuthenticationView.as_view(), name="Authentication View"),
     path("refresh-token", TokenRefreshView.as_view(), name="Refresh Token"),
-    path("logout", LogoutView.as_view(), name="Logout View"),
+    path("sign-out", LogoutView.as_view(), name="Logout View"),
     path("integration/", include("integration.urls")),
     path("inbox/", include("inbox.urls")),
     path("webhook/", include("webhook.urls")),
