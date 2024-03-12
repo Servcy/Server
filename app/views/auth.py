90422924f8777fe4275b8f1c90e8dcd25ef08fb4
Megham Garg <gargmegham3@gmail.com>
2024-03-12 14:52:09 +0530

    Refactor AuthenticationView in auth.py

--- app/views/auth.py
+++ app/views/auth.py
@@ -3,7 +3,6 @@ import logging
 from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from twilio.base.exceptions import TwilioRestException
 from twilio.rest import Client
 
@@ -12,13 +11,14 @@ from app.utils.auth import (
     send_authentication_code_phone,
 )
 from common.responses import error_response, success_response
+from common.views import BaseAPIView
 from iam.serializers import JWTTokenSerializer
 from iam.services.accounts import AccountsService
 
 logger = logging.getLogger(__name__)
 
 
-class AuthenticationView(APIView):
+class AuthenticationView(BaseAPIView):
     authentication_classes = []
     permission_classes = []
 
