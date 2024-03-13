29d2732865ddfa9952616b35cd54e412ead0a735
Megham Garg <gargmegham3@gmail.com>
2024-03-13 09:09:15 +0530

    Remove TokenRefreshView from app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,10 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenRefreshView
 
 from app.views.auth import AuthenticationView
 from app.views.logout import LogoutView
 
 urlpatterns = [
     path("authentication", AuthenticationView.as_view(), name="Authentication View"),
-    path("refresh-token", TokenRefreshView.as_view(), name="Refresh Token"),
     path("logout", LogoutView.as_view(), name="Logout View"),
     path("integration/", include("integration.urls")),
     path("inbox/", include("inbox.urls")),
