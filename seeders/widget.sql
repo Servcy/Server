08fbad6189c70370b1848935f3c629d96f13f18a
Megham Garg <gargmegham3@gmail.com>
2024-03-15 11:57:03 +0530

    widget seeder

---
+++ seeders/widget.sql
@@ -0,0 +1,8 @@
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.729', '2024-03-15 11:55:17.729', 'recent_collaborators', '{}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.740', '2024-03-15 11:55:17.740', 'recent_projects', '{}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.741', '2024-03-15 11:55:17.741', 'recent_activity', '{}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.741', '2024-03-15 11:55:17.741', 'issues_by_priority', '{"duration": "none"}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.742', '2024-03-15 11:55:17.742', 'issues_by_state_groups', '{"duration": "none"}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.742', '2024-03-15 11:55:17.742', 'created_issues', '{"tab": "pending", "duration": "none"}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.743', '2024-03-15 11:55:17.743', 'assigned_issues', '{"tab": "pending", "duration": "none"}'::jsonb);
+INSERT INTO widget (created_at, updated_at, "key", filters) VALUES('2024-03-15 11:55:17.743', '2024-03-15 11:55:17.743', 'overview_stats', '{}'::jsonb);
