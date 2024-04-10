54f1fc408518cc1368468e0e1d3f22fe9fe745d6
Megham Garg <gargmegham3@gmail.com>
2024-04-10 08:09:10 +0530

    Fix import error in oauth.py

--- integration/views/oauth.py
+++ integration/views/oauth.py
@@ -2,7 +2,7 @@ import logging
 import urllib.parse
 
 from rest_framework.decorators import action
-
+from rest_framework import status
 from common.responses import error_response, success_response
 from common.views import BaseViewSet
 from integration.services.asana import AsanaService
