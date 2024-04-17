5ae921e90beb70c14e30e4fcbf0ecaf108e4e789
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2024-04-17 13:05:22 +0530

    Razorpay integration (#39)
    * Add Razorpay payment gateway integration
    
    * Refactor billing URLs and views to handle workspace subscriptions and add Razorpay payment gateway integration
    
    * Refactor billing views to include workspace information in subscription creation
    
    * Refactor billing views to include workspace information in subscription creation and handle Razorpay payment gateway integration

--- billing/views.py
+++ billing/views.py
@@ -1,9 +1,12 @@
 from rest_framework.response import Response
-
+import razorpay
 from billing.models import Subscription
+from iam.models import Workspace
 from billing.serializers import SubscriptionSerializer
 from common.permissions import WorkspaceUserPermission
-from common.views import BaseAPIView
+from common.views import BaseAPIView, BaseViewSet
+from common.billing import PLAN_LIMITS
+from django.conf import settings
 
 
 class WorkspaceSubscriptionView(BaseAPIView):
@@ -28,9 +31,7 @@ class WorkspaceSubscriptionView(BaseAPIView):
                     "plan_details": {
                         "name": "Starter Plan",
                     },
-                    "limits": {
-                        "invitations": 5,
-                    },
+                    "limits": PLAN_LIMITS["starter"],
                     "valid_till": None,
                     "subscription_details": {},
                     "is_active": True,
@@ -38,3 +39,61 @@ class WorkspaceSubscriptionView(BaseAPIView):
                 }
             )
         )
+
+
+class RazorpayView(BaseViewSet):
+    """
+    This view will handle the razorpay payment gateway
+    """
+
+    _api_key = settings.RAZORPAY_API_KEY
+    _api_secret = settings.RAZORPAY_API_SECRET
+
+    permission_classes = [WorkspaceUserPermission]
+
+    def post(self, request, workspace_slug):
+        """
+        This method will create a subscription and return the subscription details
+        """
+        client = razorpay.Client(auth=(self._api_key, self._api_secret))
+        plan_id = request.data.get("plan_id")
+        if not plan_id:
+            return Response(
+                {"error": "Please provide the plan_id to create a subscription"},
+                status=400,
+            )
+        plan = client.plan.fetch(plan_id)
+        if Subscription.objects.filter(
+            workspace__slug=workspace_slug, is_active=True
+        ).exists():
+            return Response(
+                {"error": "Subscription already exists for the workspace"},
+                status=400,
+            )
+        workspace = Workspace.objects.get(slug=workspace_slug)
+        subscription = client.subscription.create(
+            data={
+                "plan_id": plan["id"],
+                "customer_notify": 1,  # Notify the customer about the subscription
+                "total_count": 60,  # 60 months
+            }
+        )
+        Subscription.objects.create(
+            plan_details=plan,
+            workspace=workspace,
+            subscription_details=subscription,
+            limits=PLAN_LIMITS[str(plan["item"]["name"]).lower()],
+            valid_till=None,
+            is_active=False,
+            created_by=request.user,
+            updated_by=request.user,
+        )
+        return Response(subscription)
+
+    def get(self, request, workspace_slug):
+        """
+        This method will return the available subscription plans
+        """
+        client = razorpay.Client(auth=(self._api_key, self._api_secret))
+        plans = client.plan.all()
+        return Response(plans)
---
+++ common/billing.py
@@ -0,0 +1,6 @@
+PLAN_LIMITS = {
+    "plus": {"invitations": 25},
+    "business": {"invitations": 50},
+    "enterprise": {"invitations": 100},
+    "starter": {"invitations": 5},
+}
