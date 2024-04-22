e0fd6e778ce3818c871cff1ae93945a7d03a2749
Megham Garg <gargmegham3@gmail.com>
2024-04-22 15:02:51 +0530

    Add timer URLs to project URLs

--- project/urls/__init__.py
+++ project/urls/__init__.py
@@ -7,6 +7,7 @@ from .page import urlpatterns as page_urls
 from .project import urlpatterns as project_urls
 from .search import urlpatterns as search_urls
 from .state import urlpatterns as state_urls
+from .timer import urlpatterns as timer_urls
 from .views import urlpatterns as view_urls
 from .workspace import urlpatterns as workspace_urls
 
@@ -20,6 +21,7 @@ urlpatterns = [
     *module_urls,
     *page_urls,
     *project_urls,
+    *timer_urls,
     *search_urls,
     *state_urls,
     *workspace_urls,
