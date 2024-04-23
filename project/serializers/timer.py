371185ab8fe987ab39abdf363b649bdf4250d0fc
Megham Garg <gargmegham3@gmail.com>
2024-04-23 13:29:08 +0530

    Refactor TrackedTimeSerializer to include snapshots in project/serializers/timer.py

--- project/serializers/timer.py
+++ project/serializers/timer.py
@@ -1,4 +1,5 @@
 from app.serializers import ServcyBaseSerializer
+from rest_framework import serializers
 from project.models import TrackedTime, TrackedTimeAttachment
 from project.serializers import IssueUltraLiteSerializer, ProjectUltraLiteSerializer
 
@@ -6,6 +7,11 @@ from project.serializers import IssueUltraLiteSerializer, ProjectUltraLiteSerial
 class TrackedTimeSerializer(ServcyBaseSerializer):
     issue_detail = IssueUltraLiteSerializer(read_only=True, source="issue")
     project_detail = ProjectUltraLiteSerializer(read_only=True, source="project")
+    snapshots = serializers.SerializerMethodField()
+
+    def get_snapshots(self, obj):
+        snapshots = TrackedTimeAttachment.objects.filter(tracked_time=obj)
+        return TrackedTimeAttachmentSerializer(snapshots, many=True).data
 
     class Meta:
         model = TrackedTime
